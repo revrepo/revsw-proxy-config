e156581ce83c40b03c71f3fd7cc8696f5ce3cc5f
Strajan Sebastian <strajan.sebastian@gmail.com>
2015-11-23 19:33:31 +0000

    integrate new set of tests; more will follow

--- tests/proxy-qa-bp-varnish-ttl_and_grace.js
+++ tests/proxy-qa-bp-varnish-ttl_and_grace.js
@@ -22,6 +22,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var should = require('should-http');
 var request = require('supertest');
 
+var url = 'http://testsjc20-bp01.revsw.net';
 
 /// REWRITE FUNCTION - THIS WILL treat the follwing casese
 
@@ -29,41 +30,43 @@ var request = require('supertest');
 //// TEST GRACE VALUE AND TTL VALUE
 //// TEST THE RESPONSE OF THE BACKEND SERVERS after the resource enters in grace period and exists from the grace period
 
-
-function second_test_batch(generic_object)
+function test_cache_time(generic_object)
 {
 	var operators = {
 		'<': function(a, b) { return a < b },
 		'>=': function(a, b) { return a >= b }
 	};
-	var final_description = 'Header Test - BP header ' + generic_object['action'] + ' function works - ' + generic_object['description'];
+	var final_description = 'Caching Time Test - ' + generic_object['action'] + ' function works - testing ' + generic_object['desc'];
 
 	it(final_description, function(done) {
 		request(url)
 			.get(generic_object['get_obj'])
 			.set('Host', domain_header)
-			.expect('Content-Type', generic_object['content_type'])
-			.expect(generic_object['base_header_key'], generic_object['base_header_value'])
+			.expect('Content-Type', /text/)
+			.expect(generic_object['csk'], generic_object['csv'])
+			.expect(generic_object['chk'], generic_object['chv'])
 			.end(function(err, res) {
+
+console.log(res.headers);
 				if (err) {
 					throw err;
 				}
-				res.should.have.status(200);
-				if (generic_object['action'] === 'ADD') {
-					// nothing special needs to be done for add
-				} else if (generic_object['action'] === 'REPLACE') {
-					// nothing special needs to be done for replace
-				} else if (generic_object['action'] === 'DELETE') {
-					// add here extra code
-					var response_json = JSON.stringify(res.header);
-					var i = response_json.search(generic_object['base_delete_header_value']);
-					var delete_problem = operators[generic_object['delete_op']](i, 1);
-					if (delete_problem) {
-						throw new Error('Delete Broken');
-					}
-				} else {
-					throw new Error('Unknow type of check action for second test: [' + generic_object['action'] + ']');
-				}
+				res.should.have.status(generic_object['sc']);
+//				if (generic_object['action'] === 'ADD') {
+//					// nothing special needs to be done for add
+//				} else if (generic_object['action'] === 'REPLACE') {
+//					// nothing special needs to be done for replace
+//				} else if (generic_object['action'] === 'DELETE') {
+//					// add here extra code
+//					var response_json = JSON.stringify(res.header);
+//					var i = response_json.search(generic_object['base_delete_header_value']);
+//					var delete_problem = operators[generic_object['delete_op']](i, 1);
+//					if (delete_problem) {
+//						throw new Error('Delete Broken');
+//					}
+//				} else {
+//					throw new Error('Unknow type of check action for second test: [' + generic_object['action'] + ']');
+//				}
 				done();
 			});
 	});
@@ -72,22 +75,14 @@ function second_test_batch(generic_object)
 // add a new domain and continue
 
 describe('Headers Manipulation Test - Varnish specific resource - specific headers', function() {
-	var url = 'http://testsjc20-bp01.revsw.net';
-	var envdump = '/cgi-bin/envtest.cgi';
-	var resource_path_1 = '/resource_path_1';
-	var resource_path_2 = '/resource_path_2';
-	var resource_path_3 = '/resource_path_3';
+	var fr = '/fictive_resource.html';
 	var domain_header = 'test-proxy-headers.revsw.net';
-	var test_object_js_1 = '/test_object_purge_api01.js';
 
-///# 	second_test_batch({
-///# 		'get_obj': test_object_js_1,
-///# 		'content_type': /javascript/,
-///# 		'action': 'ADD',
-///# 		'description': 'This-Is-A-Test',
-///# 		'base_header_key': 'This-Is-A-Test',
-///# 		'base_header_value': /Value-One/
-///# 	});
+	// 200 -> wait for TTL 
+
+
+///////////////////////////////////////////// cache_status ---- miss hit error
+	test_cache_time({ 'get_obj': fr, 'action': 'TTL', 'sc': 200, 'csk': 'X-Rev-Cache', 'csv': /MISS/, 'chk': 'X-Rev-Cache-Hits', 'chv': '1', 'desc': 'Testing first GET is done with success' });
 ///# 	second_test_batch({
 ///# 		'get_obj': test_object_js_1,
 ///# 		'content_type': /javascript/,
