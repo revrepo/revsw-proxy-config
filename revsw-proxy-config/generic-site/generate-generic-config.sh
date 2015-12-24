604cc0b0b8ff93507e4c7d9115e0aadd6ae6daf4
TheDialord <TheDialord@github.com>
2015-12-24 10:54:53 +0500

    generic configuration fixes

--- revsw-proxy-config/generic-site/generate-generic-config.sh
+++ revsw-proxy-config/generic-site/generate-generic-config.sh
@@ -9,16 +9,15 @@ OUT_DIR=../../generic-site
 
 mkdir -p $OUT_DIR
 
-# BP generic config
 cat <<EOF |
-bps bp-generic.1234
+bps proxy_config-generic.1234
 domain generic-domain.1234 \
 	ows ows-generic-server.1234 \
 	ows-domain ows-generic-domain.1234 \
 	optimizers $COs \
 	http https
 EOF
-apache-gen-config-script.py --no-flush --no-send --copy-to $OUT_DIR/proxy_config.json
+apache-gen-config-script.py --no-flush --no-send --no-co --copy-to $OUT_DIR/proxy_config.json
 
 chmod +x configure.sh
 ./configure.sh
