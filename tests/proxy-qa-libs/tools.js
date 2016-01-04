751020443df9fd90bd3a709ed8190c2dae9e6d44
djkobraz <djkobraz@gmail.com>
2016-01-04 04:47:32 +0200

    added freshly domain test

--- tests/proxy-qa-libs/tools.js
+++ tests/proxy-qa-libs/tools.js
@@ -8,10 +8,78 @@ var async = require('async');
 module.exports = {
 
     // Get content by url and get request with special Host
-    getHostRequest: function (url, get, set) {
+    getHostRequest: function (url, get, set, expect) {
+        expect = expect || 200
         return new Promise(function (resolve, reject) {
             return request(url)
                 .get(get)
+                .set('Host', set)
+                .expect(expect)
+                .end(function (err, res) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(res);
+                });
+        });
+    },
+
+    patchHostRequest: function (url, post, body, set, expect) {
+        expect = expect || 200
+        return new Promise(function (resolve, reject) {
+            return request(url)
+                .patch(post)
+                .set('Host', set)
+                .send(body)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(res);
+                });
+        });
+    },
+
+    postHostRequest: function (url, post, body, set, expect) {
+        expect = expect || 200
+        return new Promise(function (resolve, reject) {
+            return request(url)
+                .post(post)
+                .set('Host', set)
+                .send(body)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(res);
+                });
+        });
+    },
+
+    putHostRequest: function (url, put, body, set, expect) {
+        expect = expect || 200
+        return new Promise(function (resolve, reject) {
+            return request(url)
+                .put(put)
+                .set('Host', set)
+                .send(body)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(res);
+                });
+        });
+    },
+
+    delHostRequest: function (url, del, set, expect) {
+        expect = expect || 200
+        return new Promise(function (resolve, reject) {
+            return request(url)
+                .del(del)
                 .set('Host', set)
                 .expect(200)
                 .end(function (err, res) {
