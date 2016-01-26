bb6efca19e5855882189b4e74e709de2acaf2e80
djkobraz <djkobraz@gmail.com>
2016-01-26 04:38:45 +0200

    sleep updated

--- tests/proxy-qa-libs/util.js
+++ tests/proxy-qa-libs/util.js
@@ -1,14 +1,10 @@
 'use strict';
 
 module.exports = {
-
   mySleep: function (milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds) {
-        break;
+    var e = new Date().getTime() + (milliseconds);
+      while (new Date().getTime() <= e) {
+        ;
       }
-    }
   }
-
 };
