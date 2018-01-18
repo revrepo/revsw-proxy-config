93348d76cc0fe016d0820240517c70f4ea80e1fc
Victor Gartvich <victor@revsw.com>
2018-01-18 02:03:06 +0000

    [ci skip] Fix for Wallarm test

--- tests/proxy-wallarm-check.js
+++ tests/proxy-wallarm-check.js
@@ -1,6 +1,6 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-var config = require('config'););
+var config = require('config');
 var tools = require('./proxy-qa-libs/tools.js');
 var util = require('./proxy-qa-libs/util.js');
 
@@ -11,7 +11,7 @@ describe('Proxy Wallarm control', function () {
 
   this.timeout(500000);
   it('should make http request and return status 200', function (done) {
-    tools.getHostRequest(testHTTPSUrl, "/wallarm_monitor", testDomain).then(function (res, rej) {
+    tools.getHostRequest(testHTTPSUrl, '/wallarm_monitor', testDomain).then(function (res, rej) {
       if (rej) {
         throw rej;
       }
@@ -22,7 +22,7 @@ describe('Proxy Wallarm control', function () {
   });
 
   it('should not block the location', function (done) {
-    tools.getHostRequest(testHTTPSUrl, "/wallarm_sqli", testDomain).then(function (res, rej) {
+    tools.getHostRequest(testHTTPSUrl, '/wallarm_sqli', testDomain).then(function (res, rej) {
       if (rej) {
         throw rej;
       }
@@ -33,7 +33,7 @@ describe('Proxy Wallarm control', function () {
   });
 
   it('should block the location and return status code 403', function (done) {
-    tools.getHostRequest(testHTTPSUrl, "/wallarm_sqli?id=10 UNION SELECT 1,null,null--", testDomain, 403).then(function (res, rej) {
+    tools.getHostRequest(testHTTPSUrl, '/wallarm_sqli?id=10 UNION SELECT 1,null,null--', testDomain, 403).then(function (res, rej) {
       if (rej) {
         throw rej;
       }
@@ -44,7 +44,7 @@ describe('Proxy Wallarm control', function () {
   });
 
   it('should not block the location', function (done) {
-    tools.getHostRequest(testHTTPSUrl, "/wallarm_xss", testDomain).then(function (res, rej) {
+    tools.getHostRequest(testHTTPSUrl, '/wallarm_xss', testDomain).then(function (res, rej) {
       if (rej) {
         throw rej;
       }
@@ -55,7 +55,7 @@ describe('Proxy Wallarm control', function () {
   });
 
   it('should block the location and return status code 403', function (done) {
-    tools.getHostRequest(testHTTPSUrl, "/wallarm_xss?id='or+1=1--a-<script>prompt(1)</script>", testDomain, 403).then(function (res, rej) {
+    tools.getHostRequest(testHTTPSUrl, '/wallarm_xss?id=\'or+1=1--a-<script>prompt(1)</script>', testDomain, 403).then(function (res, rej) {
       if (rej) {
         throw rej;
       }
@@ -66,7 +66,7 @@ describe('Proxy Wallarm control', function () {
   });
 
   it('should return status code 403', function (done) {
-    tools.getHostRequest(testHTTPSUrl, "/wallarm_block", testDomain, 403).then(function (res, rej) {
+    tools.getHostRequest(testHTTPSUrl, '/wallarm_block', testDomain, 403).then(function (res, rej) {
       if (rej) {
         throw rej;
       }
